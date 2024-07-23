e1cb68def74c032dcdaca63200c5851fcf4bd17d
HamidSherz <hamidsherzai123@gmail.com>
2024-07-23 21:37:28 +0100

    Delete check_unique_records.sql

--- check_unique_records.sql
+++
@@ -1,7 +0,0 @@
-/* 
-Counted total records and distinct client numbers in the bankchurners table to assess data uniqueness and identify potential duplicates.
-*/
-SELECT
-COUNT(*) AS total_records,
-COUNT(DISTINCT CLIENTNUM) AS distinct_records
-FROM bankchurners;
